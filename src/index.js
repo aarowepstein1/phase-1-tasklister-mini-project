8885d1710b1ef44ac356a27fd7212d845ffb8826
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-16 19:54:43 -0700

    commit tree d75cdb2ddf7327bef627796c0416b3dbfc564a41

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+   const form = document.querySelector('form');
+   form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset()
+   })
+})
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  p.textContent = `${todo} `;
+  document.querySelector('#list').appendChild(p);
+  p.appendChild(btn);
+  btn.addEventListener('click', handleDelete);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
